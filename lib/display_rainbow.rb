3011d0934153e79ba577e744794416beec693763
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-23 03:34:05 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,6 @@
 
 def display_rainbow(colors)
 
-  puts "#{colors[0].uppercase.first}: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  puts "#{colors[0].first.uppercase}: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
 end
